863f389d67a94463a25346e6c3858d7a9847d346
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-10-04 03:39:59 +0200

    Мегапуперпул на тест

--- upload/catalog/language/ru-ru/extension/captcha/basic_captcha.php
+++ upload/catalog/language/ru-ru/extension/captcha/basic_captcha.php
@@ -1,11 +1,11 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2020.
+// *	@forum		http://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
-// Heading
-$_['heading_title'] = 'Капча';
+// Text
+$_['text_captcha']  = 'Капча';
 
 // Entry
 $_['entry_captcha'] = 'Введите код в поле ниже';
