3bae014f36eb4f81b030132f6d4a59c1914660d7
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-10-04 04:27:50 +0200

    Мегапуперпул на тест

--- upload/admin/controller/extension/module/featured.php
+++ upload/admin/controller/extension/module/featured.php
@@ -1,6 +1,6 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2020.
+// *	@forum		http://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
@@ -20,6 +20,8 @@ class ControllerExtensionModuleFeatured extends Controller {
 			} else {
 				$this->model_extension_module->editModule($this->request->get['module_id'], $this->request->post);
 			}
+
+			$this->cache->delete('product');
 
 			$this->session->data['success'] = $this->language->get('text_success');
 
