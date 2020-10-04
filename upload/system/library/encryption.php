889bf44c477eddf074f3811a051ac56e2bd801b0
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-10-04 03:43:09 +0200

    Мегапуперпул на тест

--- upload/system/library/encryption.php
+++ upload/system/library/encryption.php
@@ -1,6 +1,6 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2020.
+// *	@forum		http://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
@@ -12,10 +12,20 @@ final class Encryption {
 	}
 
 	public function encrypt($value) {
-		return strtr(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, hash('sha256', $this->key, true), $value, MCRYPT_MODE_ECB)), '+/=', '-_,');
+		if (version_compare(phpversion(), '7.1.0', '>') == true || !function_exists('mcrypt_encrypt')) {
+			// для улучшения сюда: https://www.php.net/manual/ru/function.openssl-encrypt.php
+			return strtr(base64_encode(openssl_encrypt($value, 'aes-128-cbc', $this->key)), '+/=', '-_,');
+		} else {
+			return strtr(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, hash('sha256', $this->key, true), $value, MCRYPT_MODE_ECB)), '+/=', '-_,');
+		}
 	}
 
 	public function decrypt($value) {
-		return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, hash('sha256', $this->key, true), base64_decode(strtr($value, '-_,', '+/=')), MCRYPT_MODE_ECB));
+		if (version_compare(phpversion(), '7.1.0', '>') == true || !function_exists('mcrypt_encrypt')) {
+			// для улучшения сюда: https://www.php.net/manual/ru/function.openssl-encrypt.php
+			return trim(openssl_decrypt(base64_decode(strtr($value, '-_,', '+/=')), 'aes-128-cbc', $this->key));
+		} else {
+			return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, hash('sha256', $this->key, true), base64_decode(strtr($value, '-_,', '+/=')), MCRYPT_MODE_ECB));
+		}
 	}
 }
--- upload/system/library/session.php
+++ upload/system/library/session.php
@@ -1,6 +1,6 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2020.
+// *	@forum		http://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
@@ -10,32 +10,32 @@ class Session {
 
 	public function __construct($adaptor = 'native') {
 		$class = 'Session\\' . $adaptor;
-		
+
 		if (class_exists($class)) {
 			$this->adaptor = new $class($this);
 		} else {
 			throw new \Exception('Error: Could not load session adaptor ' . $adaptor . ' session!');
-		}		
-		
+		}
+
 		if ($this->adaptor) {
 			session_set_save_handler($this->adaptor);
 		}
-			
+
 		if ($this->adaptor && !session_id()) {
 			ini_set('session.use_only_cookies', 'Off');
 			ini_set('session.use_cookies', 'On');
 			ini_set('session.use_trans_sid', 'Off');
 			ini_set('session.cookie_httponly', 'On');
-		
+
 			if (isset($_COOKIE[session_name()]) && !preg_match('/^[a-zA-Z0-9,\-]{22,52}$/', $_COOKIE[session_name()])) {
 				exit('Error: Invalid session ID!');
 			}
-			
+
 			session_set_cookie_params(0, '/');
 			session_start();
-		}			
+		}
 	}
-		
+
 	public function start($key = 'default', $value = '') {
 		if ($value) {
 			$this->session_id = $value;
@@ -43,25 +43,25 @@ class Session {
 			$this->session_id = $_COOKIE[$key];
 		} else {
 			$this->session_id = $this->createId();
-		}	
-		
+		}
+
 		if (!isset($_SESSION[$this->session_id])) {
 			$_SESSION[$this->session_id] = array();
 		}
-		
+
 		$this->data = &$_SESSION[$this->session_id];
-		
+
 		if ($key != 'PHPSESSID') {
 			setcookie($key, $this->session_id, ini_get('session.cookie_lifetime'), ini_get('session.cookie_path'), ini_get('session.cookie_domain'), ini_get('session.cookie_secure'), ini_get('session.cookie_httponly'));
 		}
-		
+
 		return $this->session_id;
 	}	
 
 	public function getId() {
 		return $this->session_id;
 	}
-	
+
 	public function createId() {
 		if (version_compare(phpversion(), '5.5.4', '>') == true) {
 			return $this->adaptor->create_sid();
@@ -73,12 +73,12 @@ class Session {
 			return substr(bin2hex(mcrypt_create_iv(26, MCRYPT_DEV_URANDOM)), 0, 26);
 		}
 	}
-		
+
 	public function destroy($key = 'default') {
 		if (isset($_SESSION[$key])) {
 			unset($_SESSION[$key]);
 		}
-		
+
 		setcookie($key, '', time() - 42000, ini_get('session.cookie_path'), ini_get('session.cookie_domain'));
 	}
 }
