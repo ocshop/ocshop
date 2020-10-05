bb68240ca61cff55c6d9334678a3db4ffc146461
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-10-05 11:09:27 +0200

    Warning: A non-numeric value encountered php7+

--- upload/catalog/controller/product/search.php
+++ upload/catalog/controller/product/search.php
@@ -59,7 +59,7 @@ class ControllerProductSearch extends Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}
