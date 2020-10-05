42a9e59812f6ab67b22dc8d075d250d7d1fc3299
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-10-05 11:12:42 +0200

    Warning: A non-numeric value encountered php7+

--- upload/catalog/controller/blog/category.php
+++ upload/catalog/controller/blog/category.php
@@ -28,7 +28,7 @@ class ControllerBlogCategory extends Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 			$this->document->setRobots('noindex,follow');
 		} else {
 			$page = 1;
