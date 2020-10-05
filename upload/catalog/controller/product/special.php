79475d16ca515353d8765c7760636b6eb7b1f3a5
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-10-05 11:10:14 +0200

    Warning: A non-numeric value encountered php7+

--- upload/catalog/controller/product/special.php
+++ upload/catalog/controller/product/special.php
@@ -26,7 +26,7 @@ class ControllerProductSpecial extends Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 			$this->document->setRobots('noindex,follow');
 		} else {
 			$page = 1;
