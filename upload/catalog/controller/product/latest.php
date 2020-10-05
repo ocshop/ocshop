33f922c50eee23bfe02f07fc981e6dae7ee36392
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-10-05 11:10:49 +0200

    Warning: A non-numeric value encountered php7+

--- upload/catalog/controller/product/latest.php
+++ upload/catalog/controller/product/latest.php
@@ -30,7 +30,7 @@ class ControllerProductLatest extends Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 			$this->document->setRobots('noindex,follow');
 		} else {
 			$page = 1;
