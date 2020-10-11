64e4ad29c9f44e3f2efb9352e0cb675660027a2a
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-10-11 10:32:17 +0200

    При отключении валюты - сохранялась у покупателей
    Нужно взять за правило устанавливать спец кавычки.

--- upload/catalog/model/localisation/currency.php
+++ upload/catalog/model/localisation/currency.php
@@ -6,7 +6,7 @@
 
 class ModelLocalisationCurrency extends Model {
 	public function getCurrencyByCode($currency) {
-		$query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "currency WHERE code = '" . $this->db->escape($currency) . "'");
+		$query = $this->db->query("SELECT DISTINCT * FROM `" . DB_PREFIX . "currency` WHERE `code` = '" . $this->db->escape($currency) . "'");
 
 		return $query->row;
 	}
@@ -17,7 +17,7 @@ class ModelLocalisationCurrency extends Model {
 		if (!$currency_data) {
 			$currency_data = array();
 
-			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "currency WHERE status = '1' ORDER BY title ASC");
+			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "currency` WHERE `status` = '1' ORDER BY `title` ASC");
 
 			foreach ($query->rows as $result) {
 				$currency_data[$result['code']] = array(
