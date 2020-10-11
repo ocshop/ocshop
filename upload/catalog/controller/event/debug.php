6ca8709abab47d652de70eee62c36b5b5f00b10c
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-10-11 14:48:52 +0200

    fix php 7.0+

--- upload/catalog/controller/event/debug.php
+++ upload/catalog/controller/event/debug.php
@@ -1,24 +1,24 @@
 <?php
 // *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@forum		http://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 class ControllerEventDebug extends Controller {
 	public function before(&$route, &$data) {
-		if ($route == '') { // add the route you want to test
-			$this->session->data['debug'][$route] = microtime();
+		if ($route) {
+			$this->session->data['debug'][$route] = microtime(true);
 		}
 	}
-	
+
 	public function after(&$route, &$data, &$output) {
-		if ($route == '') { // add the route you want to test
+		if ($route) {
 			if (isset($this->session->data['debug'][$route])) {
 				$data = array(
 					'route' => $route,
-					'time'  => microtime() - $this->session->data['debug'][$route]
+					'time'  => microtime(true) - $this->session->data['debug'][$route]
 				);
-				
+
 				$this->log->write($data);
 			}
 		}
