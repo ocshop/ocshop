55f324bfcfe1d4427060a9078329c4ba54a27e84
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2020-12-17 15:28:29 +0200

    Update footer.php

--- upload/admin/language/ru-ru/common/footer.php
+++ upload/admin/language/ru-ru/common/footer.php
@@ -1,9 +1,9 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2020.
+// *	@forum		http://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 // Text
-$_['text_footer'] 	= '<a href="http://opencart.pro/">OPENCART.CMS</a> &copy; 2009-' . date('Y') . ' Все права защищены. <br /><a href="http://forum.opencart.pro">Форум Поддержки</a>';
+$_['text_footer'] 	= '<a href="https://opencart.pro/" rel="noreferrer">OPENCART.CMS</a> &copy; 2009-' . date('Y') . ' Все права защищены. <br /><a href="http://forum.opencart.pro">Форум Поддержки</a>';
 $_['text_version'] 	= 'Версия %s';
