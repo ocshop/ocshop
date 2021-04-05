fe68e1acaf1c36564e255284b1b7b087e158c2f4
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2021-04-05 22:54:37 +0200

    Update product.php

--- upload/admin/model/catalog/product.php
+++ upload/admin/model/catalog/product.php
@@ -512,7 +512,9 @@ class ModelCatalogProduct extends Model {
 					$implode_data[] = "p2c.category_id = '" . (int)$category['category_id'] . "'";
 				}
 
-				$sql .= " AND (" . implode(' OR ', $implode_data) . ")";
+				if ($implode_data) {
+					$sql .= " AND (" . implode(' OR ', $implode_data) . ")";
+				}
 			} else {
 				if ((int)$data['filter_category'] > 0) {
 					$sql .= " AND p2c.category_id = '" . (int)$data['filter_category'] . "'";
@@ -871,7 +873,9 @@ class ModelCatalogProduct extends Model {
 					$implode_data[] = "p2c.category_id = '" . (int)$category['category_id'] . "'";
 				}
 
-				$sql .= " AND (" . implode(' OR ', $implode_data) . ")";
+				if ($implode_data) {
+					$sql .= " AND (" . implode(' OR ', $implode_data) . ")";
+				}
 			} else {
 				if ((int)$data['filter_category'] > 0) {
 					$sql .= " AND p2c.category_id = '" . (int)$data['filter_category'] . "'";
