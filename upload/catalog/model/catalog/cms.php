e11f340ebd1f0edc25429a7f12ac6537760789d9
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2021-04-24 06:15:12 +0200

    Add files via upload

--- upload/catalog/model/catalog/cms.php
+++ upload/catalog/model/catalog/cms.php
@@ -70,11 +70,11 @@ class ModelCatalogCms extends Model {
 		}
 
 		if (isset($data['start']) || isset($data['limit'])) {
-			if ($data['start'] < 0) {
+			if (!isset($data['start']) || $data['start'] < 0) {
 				$data['start'] = 0;
 			}
 
-			if ($data['limit'] < 1) {
+			if (!isset($data['limit']) || $data['limit'] < 1) {
 				$data['limit'] = 20;
 			}
 
@@ -109,7 +109,7 @@ class ModelCatalogCms extends Model {
 
 		$sql = "SELECT * FROM (SELECT p.product_id, p.sort_order, p.model, pd.name, p.quantity, p.price, (SELECT price FROM " . DB_PREFIX . "product_discount pd2 WHERE pd2.product_id = p.product_id AND pd2.quantity = '1' AND ((pd2.date_start = '0000-00-00' OR pd2.date_start < '" . $NOW . "') AND (pd2.date_end = '0000-00-00' OR pd2.date_end > '" . $NOW . "')) ORDER BY pd2.priority ASC, pd2.price ASC LIMIT 1) AS discount, (SELECT price FROM " . DB_PREFIX . "product_special ps WHERE ps.product_id = p.product_id  AND ((ps.date_start = '0000-00-00' OR ps.date_start < '" . $NOW . "') AND (ps.date_end = '0000-00-00' OR ps.date_end > '" . $NOW . "')) ORDER BY ps.priority ASC, ps.price ASC LIMIT 1) AS special,  (SELECT AVG(rating) AS total FROM " . DB_PREFIX . "review r1 WHERE r1.product_id = p.product_id AND r1.status = '1' GROUP BY r1.product_id) AS rating FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id) WHERE p.status = '1' AND p.date_available <= '" . $NOW . "' AND p2s.store_id = '" . (int)$this->config->get('config_store_id') .  "' AND pd.language_id = '" . (int)$this->config->get('config_language_id') . "' GROUP BY p.product_id ORDER BY p.date_added DESC, LCASE(pd.name) DESC";
 
-		$sql .= " LIMIT  0," . (int)$data['max'];
+		$sql .= " LIMIT  0," . (int)$data['limit'];
 
 		$sql .= ") p ORDER BY ";
 
@@ -141,11 +141,11 @@ class ModelCatalogCms extends Model {
 		}
 
 		if (isset($data['start']) || isset($data['limit'])) {
-			if ($data['start'] < 0) {
+			if (!isset($data['start']) || $data['start'] < 0) {
 				$data['start'] = 0;
 			}
 
-			if ($data['limit'] < 1) {
+			if (!isset($data['limit']) || $data['limit'] < 1) {
 				$data['limit'] = 20;
 			}
 
@@ -195,7 +195,7 @@ class ModelCatalogCms extends Model {
 			LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) 
 			WHERE p.status = '1' AND p.date_available <= '" . $NOW . "' 
 			AND p2s.store_id = '" . $store_id . "' 
-			GROUP BY p.product_id  ORDER by p.viewed DESC  LIMIT 0, " . (int)$data['max'];
+			GROUP BY p.product_id  ORDER by p.viewed DESC  LIMIT 0, " . (int)$data['limit'];
 
 			$sql .= ") p LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id AND pd.language_id = '". $language_id ."') ORDER BY ";
 
@@ -228,11 +228,11 @@ class ModelCatalogCms extends Model {
 			}
 
 			if (isset($data['start']) || isset($data['limit'])) {
-				if ($data['start'] < 0) {
+				if (!isset($data['start']) || $data['start'] < 0) {
 					$data['start'] = 0;
 				}
 
-				if ($data['limit'] < 1) {
+				if (!isset($data['limit']) || $data['limit'] < 1) {
 					$data['limit'] = 20;
 				}
 
