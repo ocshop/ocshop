84d08c600d6d4705ce2fc11b96a327c7e0447cd1
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2021-04-24 05:54:43 +0200

    Update login.php

--- upload/catalog/controller/account/login.php
+++ upload/catalog/controller/account/login.php
@@ -190,17 +190,21 @@ class ControllerAccountLogin extends Controller {
 
 	protected function validate() {
 		// Check how many login attempts have been made.
-		$login_info = $this->model_account_customer->getLoginAttempts($this->request->post['email']);
+		if (!isset($this->request->post['email'])) {
+			$this->error['warning'] = $this->language->get('error_login');
+		} else {
+			$login_info = $this->model_account_customer->getLoginAttempts($this->request->post['email']);
 
-		if ($login_info && ($login_info['total'] >= $this->config->get('config_login_attempts')) && strtotime('-1 hour') < strtotime($login_info['date_modified'])) {
-			$this->error['warning'] = $this->language->get('error_attempts');
-		}
+			if ($login_info && ($login_info['total'] >= $this->config->get('config_login_attempts')) && strtotime('-1 hour') < strtotime($login_info['date_modified'])) {
+				$this->error['warning'] = $this->language->get('error_attempts');
+			}
 
-		// Check if customer has been approved.
-		$customer_info = $this->model_account_customer->getCustomerByEmail($this->request->post['email']);
+			// Check if customer has been approved.
+			$customer_info = $this->model_account_customer->getCustomerByEmail($this->request->post['email']);
 
-		if ($customer_info && !$customer_info['approved']) {
-			$this->error['warning'] = $this->language->get('error_approved');
+			if ($customer_info && !$customer_info['approved']) {
+				$this->error['warning'] = $this->language->get('error_approved');
+			}
 		}
 
 		if (!$this->error) {
