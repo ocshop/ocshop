1f5454519ca85cb0e9ecaf49ac0b04e405765b8e
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2021-04-29 15:41:04 +0200

    session pro

--- upload/admin/language/en-gb/setting/setting.php
+++ upload/admin/language/en-gb/setting/setting.php
@@ -1,6 +1,6 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2020.
-// *	@forum		http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2021.
+// *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
@@ -34,6 +34,7 @@ $_['text_mail_account']              = 'Register';
 $_['text_mail_affiliate']            = 'Affiliate';
 $_['text_mail_order']                = 'Orders';
 $_['text_mail_review']               = 'Reviews';
+$_['text_session']                   = 'Warning! If, due to the settings of the sessions, it stopped letting in the admin panel or other errors, then comment out the code for setting the session settings in controller/startup/setting.php. It is recommended that you set up sessions by a professional.';
 $_['text_seo_url']                   = 'По умолчанию';
 $_['text_seo_pro']                   = 'SeoPro';
 $_['text_howtosms']                  = 'Инструкция настройки';
@@ -43,11 +44,6 @@ $_['text_upload']                    = 'Uploads';
 $_['text_error']                     = 'Error Handling';
 
 // Entry
-$_['entry_meta_title']               = 'Meta Title';
-$_['entry_meta_description']         = 'Meta Tag Description';
-$_['entry_meta_keyword']             = 'Meta Tag Keywords';
-$_['entry_layout']                   = 'Default Layout';
-$_['entry_theme']                    = 'Theme';
 $_['entry_name']                     = 'Store Name';
 $_['entry_owner']                    = 'Store Owner';
 $_['entry_address']                  = 'Address';
@@ -59,6 +55,11 @@ $_['entry_image']                    = 'Image';
 $_['entry_open']                     = 'Opening Times';
 $_['entry_comment']                  = 'Comment';
 $_['entry_location']                 = 'Store Location';
+$_['entry_meta_title']               = 'Meta Title';
+$_['entry_meta_description']         = 'Meta Tag Description';
+$_['entry_meta_keyword']             = 'Meta Tag Keywords';
+$_['entry_layout']                   = 'Default Layout';
+$_['entry_theme']                    = 'Theme';
 $_['entry_country']                  = 'Country';
 $_['entry_zone']                     = 'Region / State';
 $_['entry_language']                 = 'Language';
@@ -123,8 +124,23 @@ $_['entry_mail_alert']               = 'Alert Mail';
 $_['entry_mail_alert_email']         = 'Additional Alert Mail';
 $_['entry_secure']                   = 'Use SSL';
 $_['entry_shared']                   = 'Use Shared Sessions';
+$_['entry_session_engine']           = 'Session Type';
+$_['entry_session_name']             = 'Session cookie name';
+$_['entry_session_prefix']           = 'Session filename prefix';
+$_['entry_session_bits_per_char']    = 'Number of bits per character';
+$_['entry_session_length']           = 'Session name length';
+$_['entry_session_lifetime']         = 'Session cookie lifetime';
+$_['entry_session_maxlifetime']      = 'Maximum session cookie lifetime';
+$_['entry_session_other_lifetime']   = 'The lifetime of other cookies';
+$_['entry_session_probability']      = 'Probability';
+$_['entry_session_divisor']          = 'Divisor';
+$_['entry_session_samesite']         = 'SameSite session cookie attribute';
+$_['entry_session_other_samesite']   = 'SameSite attribute for other cookies';
+$_['entry_session_sameparty']        = 'SameParty session cookie attribute';
+$_['entry_session_other_sameparty']  = 'SameParty attribute for other cookies';
 $_['entry_robots']                   = 'Robots';
 $_['entry_seo_url']                  = 'Use SEO URLs';
+$_['entry_seo_url_type']             = 'URLs type';
 $_['entry_seo_url_cache']            = 'Enable URLs cache';
 $_['entry_seo_url_include_path']     = 'Friendly URL goods categories';
 $_['entry_blog_full_path']           = 'URL blog with categories';
@@ -196,9 +212,23 @@ $_['help_mail_alert']                = 'Select which features you would like to 
 $_['help_mail_alert_email']          = 'Any additional emails you want to receive the alert email, in addition to the main store email. (comma separated).';
 $_['help_secure']                    = 'To use SSL check with your host if a SSL certificate is installed and add the SSL URL to the catalog and admin config files.';
 $_['help_shared']                    = 'Try to share the session cookie between stores so the cart can be passed between different domains.';
+$_['help_session_engine']            = 'Native by default. Choose how you will store sessions.<br />DataBase - mysql processes sessions and stores cookies in the session table.<br />File - processes sessions as cache files and stores cookies in the folder: \'' . str_replace(DIR_SYSTEM, 'system/', DIR_SESSION) . '\'.<br />Native - sessions are handled by the SessionHandler class and cookies are stored in their path.';
+$_['help_session_name']              = 'The default is PHPSESSID. Here you can specify your own name for the cookie responsible for storing the name of the session file.';
+$_['help_session_prefix']            = 'By default sess_. The session file name prefix is ​​intended to make it difficult for an attacker to find the session file to retrieve its contents.';
+$_['help_session_bits_per_char']     = 'The default is 4. Available values:<br />4 - (0-9, a-f);<br />5 - (0-9, a-v);<br />6 - (0-9, a-z, A-Z, \'-\', \',\'). The more bits, the stronger the session identifier. In most environments, 5 is recommended.';
+$_['help_session_length']            = 'The default is 32. The length of the session name is counted with the length of the prefix. The maximum length for session types Native and File is 128 characters, and for DataBase 255. A minimum of 32 characters is recommended.';
+$_['help_session_lifetime']          = 'Default 0. Session cookie lifetime is set in seconds. 0 - means that the session will be invalid after closing the browser and will be deleted on the next visit.';
+$_['help_session_maxlifetime']       = 'The default is 1440 seconds (24 minutes). The lifetime of a session cookie is specified in seconds. Specify the maximum time threshold after which the session will be discarded by the garbage collector. It is advisable to set the time not less than the lifetime of the session cookie.';
+$_['help_session_other_lifetime']    = 'The default is 2592000 seconds (1 month). The lifetime of other cookies is set in seconds. 0 - means that cookies will be invalid after closing the browser and deleted on subsequent visit.';
+$_['help_session_probability']       = 'Default 1. This setting is intended for the garbage collector, so as not to burden the server with constant requests to delete old session files. The probability is indicated in whole numbers. Example, 1/1000 - where 1 is the probability, 1000 is the divisor, that is, the chance to remove garbage when loading the page is 1 to 1000.';
+$_['help_session_divisor']           = 'The default is 1000. This setting is intended for the garbage collector, so as not to burden the server with constant requests to delete old session files. The divisor is indicated in integers. Example, 1/1000 - where 1 is the probability, 1000 is the divisor, that is, the chance to remove garbage when loading the page is 1 to 1000.';
+$_['help_session_samesite']          = 'Default Strict<br />None - Cookies will be sent in all contexts, i.e in responses to both first-party and cross-origin requests.If SameSite=None is set, the cookie Secure attribute must also be set (or the cookie will be blocked).<br />Lax - Cookies are not sent on normal cross-site subrequests (for example to load images or frames into a third party site), but are sent when a user is navigating to the origin site (i.e. when following a link).<br />Strict - Cookies will only be sent in a first-party context and not be sent along with requests initiated by third party websites.';
+$_['help_session_other_samesite']    = 'Default Lax<br />None - Cookies will be sent in all contexts, i.e in responses to both first-party and cross-origin requests.If SameSite=None is set, the cookie Secure attribute must also be set (or the cookie will be blocked).<br />Lax - Cookies are not sent on normal cross-site subrequests (for example to load images or frames into a third party site), but are sent when a user is navigating to the origin site (i.e. when following a link).<br />Strict - Cookies will only be sent in a first-party context and not be sent along with requests initiated by third party websites.';
+$_['help_session_sameparty']         = 'Default No<br />';
+$_['help_session_other_sameparty']   = 'Default No<br />';
 $_['help_robots']                    = 'A list of web crawler user agents that shared sessions will not be used with. Use separate lines for each user agent.';
 $_['help_seo_url']                   = 'To use SEO URLs, apache module mod-rewrite must be installed and you need to rename the htaccess.txt to .htaccess.';
-$_['help_seo_url_cache']             = 'Enabling the cache URLs will reduce the load on the database. With a huge quantity of goods it is recommended to disconnect. <br /> (only for SeoPro)';
+$_['help_seo_url_cache']             = 'Enabling the cache URLs will reduce the load on the database. With a huge quantity of goods it is recommended to disconnect.<br />(only for SeoPro)';
 $_['help_seo_url_include_path']      = '/category/subcategory/product<br />(only for SeoPro)';
 $_['help_blog_full_path']            = '/blog/category/subcategory<br />(only for SeoPro)';
 $_['help_manufacturers_full_path']   = '/brands/manufacturer<br />(only for SeoPro)';
