ef2b9a03758f420eaf02a85ffed4202e73abc35e
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2021-05-02 22:20:09 +0200

    Add files via upload

--- upload/catalog/controller/startup/startup.php
+++ upload/catalog/controller/startup/startup.php
@@ -1,6 +1,6 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2017.
-// *	@forum	http://forum.opencart.pro
+// *	@copyright	OPENCART.PRO 2011 - 2021.
+// *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
@@ -8,78 +8,78 @@ class ControllerStartupStartup extends Controller {
 	public function index() {
 		// Language
 		$code = '';
-		
+
 		$this->load->model('localisation/language');
-		
+
 		$languages = $this->model_localisation_language->getLanguages();
-		
+
 		if (isset($this->session->data['language'])) {
 			$code = $this->session->data['language'];
 		}
-				
+
 		if (isset($this->request->cookie['language']) && !array_key_exists($code, $languages)) {
 			$code = $this->request->cookie['language'];
 		}
-		
+
 		// Language Detection
 		if (!empty($this->request->server['HTTP_ACCEPT_LANGUAGE']) && !array_key_exists($code, $languages)) {
 			$detect = '';
-			
+
 			$browser_languages = explode(',', $this->request->server['HTTP_ACCEPT_LANGUAGE']);
-			
+
 			// Try using local to detect the language
 			foreach ($browser_languages as $browser_language) {
 				foreach ($languages as $key => $value) {
 					if ($value['status']) {
 						$locale = explode(',', $value['locale']);
-						
+
 						if (in_array($browser_language, $locale)) {
 							$detect = $key;
 							break 2;
 						}
 					}
-				}	
-			}			
-			
+				}
+			}
+
 			if (!$detect) { 
 				// Try using language folder to detect the language
 				foreach ($browser_languages as $browser_language) {
 					if (array_key_exists(strtolower($browser_language), $languages)) {
 						$detect = strtolower($browser_language);
-						
+
 						break;
 					}
 				}
 			}
-			
+
 			$code = $detect ? $detect : '';
 		}
-		
+
 		if (!array_key_exists($code, $languages)) {
 			$code = $this->config->get('config_language');
 		}
-		
+
 		if (!isset($this->session->data['language']) || $this->session->data['language'] != $code) {
 			$this->session->data['language'] = $code;
 		}
-				
+
 		if (!isset($this->request->cookie['language']) || $this->request->cookie['language'] != $code) {
-			setcookie('language', $code, time() + 60 * 60 * 24 * 30, '/', $this->request->server['HTTP_HOST']);
+			$this->session->setcookie('language', $code);
 		}
-				
+
 		// Overwrite the default language object
 		$language = new Language($code);
 		$language->load($code);
-		
+
 		$this->registry->set('language', $language);
-		
+
 		// Set the config language_id
 		$this->config->set('config_language_id', $languages[$code]['language_id']);	
 
 		// Customer
 		$customer = new Cart\Customer($this->registry);
 		$this->registry->set('customer', $customer);
-		
+
 		// Customer Group
 		if ($this->customer->isLogged()) {
 			$this->config->set('config_customer_group_id', $this->customer->getGroupId());
@@ -89,49 +89,49 @@ class ControllerStartupStartup extends Controller {
 		} elseif (isset($this->session->data['guest']) && isset($this->session->data['guest']['customer_group_id'])) {
 			$this->config->set('config_customer_group_id', $this->session->data['guest']['customer_group_id']);
 		}
-		
+
 		// Tracking Code
 		if (isset($this->request->get['tracking'])) {
-			setcookie('tracking', $this->request->get['tracking'], time() + 3600 * 24 * 1000, '/');
-		
+			$this->session->setcookie('tracking', $this->request->get['tracking']);
+
 			$this->db->query("UPDATE `" . DB_PREFIX . "marketing` SET clicks = (clicks + 1) WHERE code = '" . $this->db->escape($this->request->get['tracking']) . "'");
-		}		
-		
+		}
+
 		// Affiliate
 		$this->registry->set('affiliate', new Cart\Affiliate($this->registry));
-		
+
 		// Currency
 		$code = '';
-		
+
 		$this->load->model('localisation/currency');
-		
+
 		$currencies = $this->model_localisation_currency->getCurrencies();
-		
+
 		if (isset($this->session->data['currency'])) {
 			$code = $this->session->data['currency'];
 		}
-		
+
 		if (isset($this->request->cookie['currency']) && !array_key_exists($code, $currencies)) {
 			$code = $this->request->cookie['currency'];
 		}
-		
+
 		if (!array_key_exists($code, $currencies)) {
 			$code = $this->config->get('config_currency');
 		}
-		
+
 		if (!isset($this->session->data['currency']) || $this->session->data['currency'] != $code) {
 			$this->session->data['currency'] = $code;
 		}
-		
+
 		if (!isset($this->request->cookie['currency']) || $this->request->cookie['currency'] != $code) {
-			setcookie('currency', $code, time() + 60 * 60 * 24 * 30, '/', $this->request->server['HTTP_HOST']);
-		}		
-		
+			$this->session->setcookie('currency', $code);
+		}
+
 		$this->registry->set('currency', new Cart\Currency($this->registry));
-		
+
 		// Tax
 		$this->registry->set('tax', new Cart\Tax($this->registry));
-		
+
 		if (isset($this->session->data['shipping_address'])) {
 			$this->tax->setShippingAddress($this->session->data['shipping_address']['country_id'], $this->session->data['shipping_address']['zone_id']);
 		} elseif ($this->config->get('config_tax_default') == 'shipping') {
@@ -145,19 +145,19 @@ class ControllerStartupStartup extends Controller {
 		}
 
 		$this->tax->setStoreAddress($this->config->get('config_country_id'), $this->config->get('config_zone_id'));
-		
+
 		// Weight
 		$this->registry->set('weight', new Cart\Weight($this->registry));
-		
+
 		// Length
 		$this->registry->set('length', new Cart\Length($this->registry));
-		
+
 		// Cart
 		$this->registry->set('cart', new Cart\Cart($this->registry));
-		
+
 		// Encryption
 		$this->registry->set('encryption', new Encryption($this->config->get('config_encryption')));
-		
+
 		// OpenBay Pro
 		$this->registry->set('openbay', new Openbay($this->registry));					
 	}
